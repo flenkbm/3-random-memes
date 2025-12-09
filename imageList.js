a77c3f0d7d8b10e464a2b2b034e66e4eabad6223
flenkbm <SpamBogdanF@gmail.com>
2025-12-09 17:22:08 +0300

    small attempt of bugfix

--- imageList.js
+++ imageList.js
@@ -1,16 +1,16 @@
 images = [
     {
-    "name": "1.png",
+    "name": "1",
     "ratio": "b1-1",
     "size":"30%"
     },
     {
-    "name": "2.png",
+    "name": "2",
     "ratio": "b2-1",
     "size":"30%"
     },
     {
-    "name": "3.png",
+    "name": "3",
     "ratio": "b1-2",
     "size":"20%"
     }
--- imagePicker3000.js
+++ imagePicker3000.js
@@ -6,17 +6,17 @@ i3 = images[Math.floor(Math.random() * images.length)];
 boxes = document.getElementsByClassName("box");
 console.log(boxes);
 //
-boxes[0].setAttribute("onclick", "HidePopup"+i1.name);
+boxes[0].setAttribute("onclick", "ShowPopup("+i1.name+")");
 boxes[0].setAttribute("style", "width: "+i1.size+";");
 boxes[0].classList.add(i1.ratio);
-boxes[0].children[0].setAttribute("src", "img/"+i1.name);
+boxes[0].children[0].setAttribute("src", "img/"+i1.name+".png");
 //
-boxes[1].setAttribute("onclick", "HidePopup"+i2.name);
+boxes[1].setAttribute("onclick", "ShowPopup("+i2.name+")");
 boxes[1].setAttribute("style", "width: "+i2.size+";");
 boxes[1].classList.add(i2.ratio);
-boxes[1].children[0].setAttribute("src", "img/"+i2.name);
+boxes[1].children[0].setAttribute("src", "img/"+i2.name+".png");
 //
-boxes[2].setAttribute("onclick", "HidePopup"+i3.name);
+boxes[2].setAttribute("onclick", "ShowPopup("+i3.name+")");
 boxes[2].setAttribute("style", "width: "+i3.size+";");
 boxes[2].classList.add(i3.ratio);
-boxes[2].children[0].setAttribute("src", "img/"+i3.name);
+boxes[2].children[0].setAttribute("src", "img/"+i3.name+".png");
