1e3efaa49e4be8ff4735a7d208bf3a8c8bb508ad
flenkbm <SpamBogdanF@gmail.com>
2025-12-11 08:54:45 +0300

    small stuff

--- imageList.js
+++ imageList.js
@@ -2,16 +2,19 @@ images = [
     {
     "name": "1",
     "ratio": "b1-1",
-    "size":"30%"
+    "size":"25%",
+    "type":".png"
     },
     {
     "name": "2",
     "ratio": "b2-1",
-    "size":"30%"
+    "size":"35%",
+    "type":".png"
     },
     {
     "name": "3",
     "ratio": "b1-2",
-    "size":"20%"
+    "size":"19%",
+    "type":".png"
     }
 ]
--- imagePicker3000.js
+++ imagePicker3000.js
@@ -1,22 +1,21 @@
-//somehow getting the json file
 console.log(images);
 i1 = images[Math.floor(Math.random() * images.length)];
 i2 = images[Math.floor(Math.random() * images.length)];
 i3 = images[Math.floor(Math.random() * images.length)];
-boxes = document.getElementsByClassName("box");
-console.log(boxes);
+img_cont = document.getElementsByClassName("img_cont");
+console.log(img_cont);
 //
-boxes[0].setAttribute("onclick", "ShowPopup("+i1.name+")");
-boxes[0].setAttribute("style", "width: "+i1.size+";");
-boxes[0].classList.add(i1.ratio);
-boxes[0].children[0].setAttribute("src", "img/"+i1.name+".png");
+img_cont[0].setAttribute("onclick", "ShowPopup("+i1.name+")");
+img_cont[0].setAttribute("style", "width: "+i1.size+";");
+img_cont[0].classList.add(i1.ratio);
+img_cont[0].children[0].setAttribute("src", "img/"+i1.name+i1.type);
 //
-boxes[1].setAttribute("onclick", "ShowPopup("+i2.name+")");
-boxes[1].setAttribute("style", "width: "+i2.size+";");
-boxes[1].classList.add(i2.ratio);
-boxes[1].children[0].setAttribute("src", "img/"+i2.name+".png");
+img_cont[1].setAttribute("onclick", "ShowPopup("+i2.name+")");
+img_cont[1].setAttribute("style", "width: "+i2.size+";");
+img_cont[1].classList.add(i2.ratio);
+img_cont[1].children[0].setAttribute("src", "img/"+i2.name+i1.type);
 //
-boxes[2].setAttribute("onclick", "ShowPopup("+i3.name+")");
-boxes[2].setAttribute("style", "width: "+i3.size+";");
-boxes[2].classList.add(i3.ratio);
-boxes[2].children[0].setAttribute("src", "img/"+i3.name+".png");
+img_cont[2].setAttribute("onclick", "ShowPopup("+i3.name+")");
+img_cont[2].setAttribute("style", "width: "+i3.size+";");
+img_cont[2].classList.add(i3.ratio);
+img_cont[2].children[0].setAttribute("src", "img/"+i3.name+i1.type);
