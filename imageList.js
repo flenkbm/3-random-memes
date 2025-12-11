f52fec4554ab2e6e74bc602bde1c4243dd6d7ef5
flenkbm <SpamBogdanF@gmail.com>
2025-12-11 09:02:52 +0300

    new images

--- imageList.js
+++ imageList.js
@@ -8,13 +8,157 @@ images = [
     {
     "name": "2",
     "ratio": "b2-1",
-    "size":"35%",
-    "type":".png"
+    "size":"30%",
+    "type":".jpg"
     },
     {
     "name": "3",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "4",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "5",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "6",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "7",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "8",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "9",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "10",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "11",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "12",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "13",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "14",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "15",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "16",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "17",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "18",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "19",
     "ratio": "b1-2",
-    "size":"19%",
-    "type":".png"
+    "size": "18%",
+    "type":".jpg"
+    },
+    {
+    "name": "3",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "20",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "21",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "22",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "23",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "24",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "25",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
+    },
+    {
+    "name": "26",
+    "ratio": "b2-1",
+    "size":"30%",
+    "type":".jpg"
     }
 ]
